941959f345ffb53f9b2714f4e9e25a3f4fc5b3a7
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-02-02 12:36:57 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@
 variable "region" { type = string }
 variable "cluster_name" { type = string }
 variable "vpc_cidr" { type = string }
+variable "vpc_name" { type = string }
 variable "public_subnets" { type = list(string) }
 variable "private_subnets" { type = list(string) }
 variable "availability_zones" { type = list(string) }
