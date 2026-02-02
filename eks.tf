49e1294ff1c0a8db44185f620b0c12d78d00cce0
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-02-02 12:40:46 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,39 +1,39 @@
-resource "aws_iam_role" "eks" {
-  name = "eks-cluster-eks"
+resource "aws_iam_role" "eks_role" {
+  name = var.eks_role_name
 
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_iam_role_policy_attachment" "eks-AmazonEKSClusterPolicy" {
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks.name
+  role       = aws_iam_role.eks_role.name
 }
 
 resource "aws_eks_cluster" "eks_cluster" {
-  name     = "eks_cluster"
-  role_arn = aws_iam_role.eks.arn
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
 
   vpc_config {
     subnet_ids = [
-      aws_subnet.private-ap-south-1a.id,
-      aws_subnet.private-ap-south-1b.id,
-      aws_subnet.public-ap-south-1a.id,
-      aws_subnet.public-ap-south-1b.id
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id,
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
     ]
   }
 
-  depends_on = [aws_iam_role_policy_attachment.eks-AmazonEKSClusterPolicy]
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
 }
