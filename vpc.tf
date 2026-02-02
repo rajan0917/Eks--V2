c7277704e99b4da5240892c78c1f9d2c1b9158f0
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-02-02 12:35:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
-resource "aws_vpc" "aws-eks-vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = var.vpc_cidr
 
   tags = {
-    Name = "aws-eks-vpc"
+    Name = var.vpc_name
   }
 }
