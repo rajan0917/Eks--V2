4eab676a562d1d17c6476607cb93168e60a871d2
rajan0917 <132059861+rajan0917@users.noreply.github.com>
2026-02-02 14:11:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 
 variable "region" { type = string }
+variable "aws_profile" { type = string }
 variable "eks_role_name" { type = string }
 variable "cluster_name" { type = string }
 variable "vpc_cidr" { type = string }
